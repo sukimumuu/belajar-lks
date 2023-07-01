30b3eeac48b26317da838c139161234151c14646
usss <88474883+sukimumuu@users.noreply.github.com>
2023-07-01 23:13:13 +0700

    lobby

--- pariwisata/resources/views/index.blade.php
+++ pariwisata/resources/views/index.blade.php
@@ -23,7 +23,7 @@
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Destination</a>
+            <a class="nav-link" href="#destination">Destination</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Hotel</a>
@@ -44,13 +44,18 @@
   </nav>
     {{-- END NAVBAR --}}
   <div class="col-xl-12 f-screen bg-index1">
-    <div class="d-flex mb-3 position-absolute top-50 start-50 translate-middle col-xl-6 form-input-text border shadow">
-      <span class="p-2"  ><i class="fa-solid fa-magnifying-glass fa-fade fa-lg" style="color: #000000;"></i></span>
-      <input type="text" class="form-input-text p-2" placeholder="Search by country..." aria-label="Username" aria-describedby="basic-addon1">
+    <div class="mb-3 position-absolute top-50 start-50 translate-middle col-xl-6">
+      <div class="d-flex form-input-text border shadow">
+        <span class="p-2"  ><i class="fa-solid fa-magnifying-glass fa-fade fa-lg" style="color: #000000;"></i></span>
+        <input type="text" class="form-input-text p-2" placeholder="Search by country..." aria-label="Username" aria-describedby="basic-addon1">
+      </div>
+      <h2 class="fw-bold display-6 text-white text-center mt-5 stroke">
+        Explore the world with Joyvel <i class="fa-solid fa-plane-departure"></i>
+      </h2>
       {{-- <button type="submit" class="btn btn-success">Search</button> --}}
     </div>
   </div>
-    <div class="col-xl-12">
+    <div class="col-xl-12 " id="destination">
       <p class="text-center mt-5 mb-5 display-6 border-bottom col-xl-6 mx-auto">Recommended Destination</p>
       <div class="container d-block">
         {{-- 1 --}}
@@ -66,8 +71,10 @@
             <a class="icon-link text-dark" href="#">
               Dubai, Emirate of Dubai
             </a>
-            <p class="mt-2 text-justify">Dubai is a destination that combines modern culture with history, and adventure with world-class shopping and entertainment. Catch a show at the Dubai Opera, see downtown from the top of the Burj Khalifa, and spend the afternoon cruising Dubai Creek exploring the gold, textile and spice souks. If you're looking for a challenge, you can soar over the dunes in a hot air balloon, go on high-speed rides at IMG Worlds of Adventure or skydive over Palm Jumeirah.</p>
-            <span class="badge text-bg-success p-2 mt-5"><i class="fa-solid fa-hotel me-2"></i>Hotels</span>
+            <p class="mt-2 text-justify c-default">Dubai is a destination that combines modern culture with history, and adventure with world-class shopping and entertainment. Catch a show at the Dubai Opera, see downtown from the top of the Burj Khalifa, and spend the afternoon cruising Dubai Creek exploring the gold, textile and spice souks. If you're looking for a challenge, you can soar over the dunes in a hot air balloon, go on high-speed rides at IMG Worlds of Adventure or skydive over Palm Jumeirah.</p>
+            <a href="#" class="d-block nav-link italic">More.. <i class="fa-solid fa-chevron-right"></i></a>
+            <span class="badge text-bg-success p-2 mt-5 pointer"><i class="fa-solid fa-hotel me-2"></i>Hotels</span>
+            <span class="badge text-bg-warning p-2 pointer"><i class="fa-solid fa-utensils me-2"></i>Food</span>
           </div>
         </div>
         {{-- 1 --}}
@@ -84,12 +91,16 @@
             <a class="icon-link text-dark" href="#">
               Bali, Indonesia
             </a>
-            <p class="mt-2 text-justify">Bali is as beautiful as a photo on a postcard, a paradise in Indonesia that feels like a land of fantasy. Enjoy sunbathing on stretches of fine white sand, or mingle with tropical creatures as you dive along stunning coral reefs and WWII warship wrecks. On the beach, a lush forest shelters temples and wild monkeys. As an “art center”, Ubud is the perfect place to see cultural dance performances, learn to make batik or work with silver, or refresh your mind and body by joining a yoga course.</p>
+            <p class="mt-2 text-justify c-default">Bali is as beautiful as a photo on a postcard, a paradise in Indonesia that feels like a land of fantasy. Enjoy sunbathing on stretches of fine white sand, or mingle with tropical creatures as you dive along stunning coral reefs and WWII warship wrecks. On the beach, a lush forest shelters temples and wild monkeys. As an “art center”, Ubud is the perfect place to see cultural dance performances, learn to make batik or work with silver, or refresh your mind and body by joining a yoga course.</p>
+            <a href="#" class="d-block nav-link italic">More.. <i class="fa-solid fa-chevron-right"></i></a>
+            <span class="badge text-bg-success p-2 mt-3 pointer"><i class="fa-solid fa-hotel me-2"></i>Hotels</span>
+            <span class="badge text-bg-warning p-2 pointer"><i class="fa-solid fa-utensils me-2"></i>Food</span>
+            <span class="badge text-bg-info p-2 pointer"><i class="fa-solid fa-umbrella-beach me-2"></i>Beach</span>
           </div>
         </div>
         {{-- 2 --}}
         {{-- 3 --}}
-         <div class="row col-xl-12">
+         <div class="row col-xl-12 mb-5">
           <div class="col-xl-4 p-0">
             <img src="assets/img/paris.jpg" alt="" class="img-fluid">
           </div>
@@ -101,10 +112,83 @@
             <a class="icon-link text-dark" href="#">
               Paris, Ile-de-France
             </a>
-            <p class="mt-2 text-justify">Enjoy a brownie at a roadside cafe, relax after a stroll along the Seine and admire city icons like the Eiffel Tower and the Arc de Triomphe… the perfect Parisian vibe combines fun and excitement, with plenty of time to sample delicious dishes and view exhibitions at Louvre. Experience the majesty of Notre Dame, enjoy bargain shopping at the Marché aux Puces de Montreuil or, for the foodie, at the Marché Biologique Raspail, then end the day by seeing a show at the Moulin Rouge.</p>
+            <p class="mt-2 text-justify c-default">Enjoy a brownie at a roadside cafe, relax after a stroll along the Seine and admire city icons like the Eiffel Tower and the Arc de Triomphe… the perfect Parisian vibe combines fun and excitement, with plenty of time to sample delicious dishes and view exhibitions at Louvre. Experience the majesty of Notre Dame, enjoy bargain shopping at the Marché aux Puces de Montreuil or, for the foodie, at the Marché Biologique Raspail, then end the day by seeing a show at the Moulin Rouge.</p>
+            <a href="#" class="d-block nav-link italic">More.. <i class="fa-solid fa-chevron-right"></i></a>
+            <span class="badge text-bg-success p-2 mt-5 pointer"><i class="fa-solid fa-hotel me-2"></i>Hotels</span>
+            <span class="badge text-bg-warning p-2 pointer"><i class="fa-solid fa-utensils me-2"></i>Food</span>
           </div>
         </div>
         {{-- 3 --}}
+        {{-- 4 --}}
+         <div class="row col-xl-12">
+          <div class="col-xl-4 p-0">
+            <img src="assets/img/roma.jpg" alt="" class="img-fluid">
+          </div>
+          <div class="col-xl-8">
+            <p class="fs-2">
+              Roma
+            </p>
+            <i class="fa-solid fa-location-dot"></i>
+            <a class="icon-link text-dark" href="#">
+              Roma, Lazio
+            </a>
+            <p class="mt-2 text-justify c-default">You can't explore Rome in one day. The city looks like a gigantic outdoor museum exhibition hall, with an extraordinary mix of massive piazzas, open-air markets, and historical sites. Toss a coin into the Trevi Fountain, visit the Coliseum and Pantheon, then enjoy a cup of cappuccino before an afternoon of shopping at Campo de'Fiori or Via Veneto. Dig into a plate of fresh pasta, some delicious fried artichokes, or delicate oxtail soup for an extraordinary dining experience.</p>
+            <a href="#" class="d-block nav-link italic">More.. <i class="fa-solid fa-chevron-right"></i></a>
+            <span class="badge text-bg-success p-2 mt-5 pointer"><i class="fa-solid fa-hotel me-2"></i>Hotels</span>
+            <span class="badge text-bg-warning p-2 pointer"><i class="fa-solid fa-utensils me-2"></i>Food</span>
+          </div>
+        </div>
+        {{-- 4 --}}
+      </div>
+    </div>
+    
+    <div class="col-xl-12 mt-5">
+      <div class="bg-index2">
+        <div class="p-5">
+          <h2 class="display-5 fw-bold text-white">Find and book hotels at the lowest prices</h2>
+          <p class="text-white fs-4">Find the best price for every Traveloka product you need.</p>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row col-xl-12 mt-4">
+        <p class="text-center mb-5 fs-3">Most Booked Hotels</p>
+        <div class="col-xl-3">
+          <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3">
+          <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3">
+          <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3">
+          <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </main>
